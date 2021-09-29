2fb202981a218de6943895965a5e2a5874416c34
gabriel <elias@fortalezatec.com.br>
2021-09-29 17:33:03 -0300

    primeiro commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text } from 'react-native';
+
+import { Container } from './styles';
+
+import Header from "../../components/Header";
+
+function Home(){
+    return(
+        <Container>
+
+            <Header title="React Prime" />
+            
+            <Text>TELA INICIAL</Text>
+        </Container>
+    )
+}
+
+export default Home;
